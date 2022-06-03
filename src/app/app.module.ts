f3d09364c5ea2193f454956027abf796f0b6f506
unknown <kemellynasc@gmail.com>
2022-06-03 14:43:01 -0300

    implementação header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastrarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
